abc9cc448c836f8f7cdae1d5146319bc711f658f
Azmir Abdulla <princeabdulla006@gmail.com>
2025-09-26 00:38:24 +0600

    Navbar and footer upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
  
@@ -7,6 +8,12 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <div>
+        <div className='rounded-lg h-7 bg-red-500 p-7 text-white'>
+          <h1>0</h1>
+        </div>
+      </div>
+      <Footer></Footer>
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
+import Container from './Container';
 
 const Navbar = () => {
-    return (
-          <div className=" bg-purple-100 shadow-md border-b-[0.5px] border-gray-300">
-    
-      <nav className="px-8 py-4 flex justify-between items-center">
-        <h1 className="md:text-xl text-sm font-bold text-black">PH University</h1>
-        <div className="md:flex hidden items-center gap-6">
-          <a href="#" className="text-gray-600">
-            Home
-          </a>
-          <a href="#" className="text-gray-600">
-            Faculty
-          </a>
-          <a href="#" className="text-gray-600">
-            Students
-          </a>
+  return (
+    <div className=" bg-white shadow-md border-b-[0.5px] border-gray-300">
 
-          <a href="#" className="text-gray-600">
-            Contact
-          </a>
+      <Container>
+          <nav className="px-8 py-4 flex justify-between items-center">
+        <h1 className="md:text-xl text-sm font-bold text-black">CS — Ticket System</h1>
+        <div className="md:flex hidden items-center gap-6">
+          <ul className='md:flex gap-6'>
+            <li><a href="#" className="text-gray-600">Home</a></li>
+            <li> <a href="#" className="text-gray-600"> FAQ</a></li>
+            <li><a href="#" className="text-gray-600">Changelog </a></li>
+            <li><a href="#" className="text-gray-600">Blog</a></li>
+            <li><a href="#" className="text-gray-600">Download</a></li>
+            <li><a href="#" className="text-gray-600">Contact</a></li>
+          </ul>
           <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:cursor-pointer">
             + New Assignment
           </button>
         </div>
       </nav>
-     
+      </Container>
+
     </div>
-    );
+  );
 };
 
 export default Navbar;
